fc644416a1b02e227ab48427069061e8cb232ac7
Serge <serge@MacBook-Pro-Serge.local>
2020-07-15 22:45:13 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
+ /* return (
     <div>
       <div className="loader">
           <span></span>
@@ -13,6 +13,34 @@ function App() {
       </div>
       Hello, world!
       <input type = 'text'></input>
+    </div>
+  );*/
+  const [value, setValue] = React.useState('');
+  const [tasks, setTasks] = React.useState([]);
+  return (
+    <div className="App">
+     Хуй {value} хуй
+     <input type="text" value={value} onChange={e => {
+       setValue(e.currentTarget.value)
+     }} onKeyDown={e => {
+       if (e.keyCode === 13) {
+         setValue('');
+         setTasks([...tasks, value]);
+        }
+     }}></input>
+     {
+       tasks.map(task => <div key={task} onClick={()=>{
+         setTasks(tasks.filter(t=>t!==task))
+       }}>{task}</div>)
+     }
+     
+    </div>
+
+    const [timer, setTimer] = React.useState(() => { currentTime: 0 });
+    <div>
+      {
+        React.useEffect(() => { setInterval(() => { setTimer({ …timer, currentTime: timer.currentTime + 1 }) }, 1000) },[]);
+      }
     </div>
   );
 }
